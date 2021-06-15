10f5f3aebf793863efdba4f306b90463b397308c
Luke MacDonald <lmacdonald@inntopia.net>
2021-06-15 17:26:32 -0400

    Section 3, classes and methods

---
+++ MyCarBasics/math/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpFunamenetals.math
+{
+    class Calculator
+    {
+        public int AddCarYears(int a)
+        {
+            return a + 2017;
+        }
+    }
+}
